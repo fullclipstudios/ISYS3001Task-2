346280c0840376668f5679e02ca40c2756de0ed4
Brock Vandenbergh <cheesecakebird2014@gmail.com>
2025-01-23 21:47:45 +1100

    Feat: Added methods to create and sort teams
    - addPlayer(player): Adds a player to the list of players
    - generateTeams(teamSize): Randomly divides players into teams of the specified size
    - clearPlayers(): Clears the list of players

--- TeamGenerator.js
+++ TeamGenerator.js
@@ -1,1 +1,30 @@
+class TeamGenerator {
+    constructor(players = []) {
+        this.players = players; 
+    }
 
+
+    addPlayer(player) {
+        this.players.push(player);
+    }
+
+    generateTeams(teamSize) {
+        if (teamSize <= 0) {
+            throw new Error("Team size must be greater than zero.");
+        }
+
+
+        const shuffledPlayers = [...this.players].sort(() => Math.random() - 0.5);
+        const teams = [];
+
+        while (shuffledPlayers.length > 0) {
+            teams.push(shuffledPlayers.splice(0, teamSize));
+        }
+
+        return teams;
+    }
+
+    clearPlayers() {
+        this.players = [];
+    }
+} 
