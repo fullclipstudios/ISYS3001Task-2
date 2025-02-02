b4506380810e2c1fb3b1bf1b8d6895f1d528ac3a
Brock Vandenbergh <cheesecakebird2014@gmail.com>
2025-02-02 20:22:08 +1100

    Feat: Add basic functionality in FriendList for managing friends
    Basic Functionality has been added to track friendships between players.
    
    - Players can add and remove friends from their list.
    - Allows checking if two players are friends.
    - Provides a method to retrieve a player’s friend list.
    - Provides a foundation for future social features.

--- FriendList.js
+++ FriendList.js
@@ -1,1 +1,29 @@
+class FriendsList {
+    constructor() {
+        this.friends = {};
+    }
 
+    addFriend(player, friend) {
+        if (!this.friends[player]) {
+            this.friends[player] = new Set();
+        }
+        this.friends[player].add(friend);
+    }
+
+    removeFriend(player, friend) {
+        if (this.friends[player]) {
+            this.friends[player].delete(friend);
+            if (this.friends[player].size === 0) {
+                delete this.friends[player];
+            }
+        }
+    }
+
+    isFriend(player, friend) {
+        return this.friends[player]?.has(friend) || false;
+    }
+
+    getFriends(player) {
+        return this.friends[player] ? Array.from(this.friends[player]) : [];
+    }
+}
